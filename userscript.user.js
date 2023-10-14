338801250c2b6bfd630311cfebb2304f57301162
KOOKY WARRIOR <91371138+kookywarrior@users.noreply.github.com>
2023-10-14 19:22:52 +0800

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -3,13 +3,14 @@
 // @description  Let you connect to the private server
 // @icon         https://moomoo.io/img/favicon.png?v=1
 // @author       KOOKY WARRIOR
-// @version      0.1
+// @version      0.1.1
 // @match        *://*.moomoo.io/*
 // @grant        none
 // ==/UserScript==
 
-; (async () => {
+;(async () => {
 	function getLocation(href) {
+		if (!href) return null
 		var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/)
 		return (
 			match && {
@@ -25,15 +26,14 @@
 		)
 	}
 	const urlobj = getLocation(window.prompt("URL", ""))
-    console.log(urlobj.protocol)
-	
+
 	if (!urlobj?.host) return
 
 	window.PrivateServer = true
 	let ws = window.WebSocket
 	class PrivateServer extends ws {
 		constructor() {
-			super(`${urlobj.protocol.includes("https") ? "wss" : "ws"}://${urlobj.host}/server`)
+			super(`${urlobj.protocol == "https" ? "wss" : "ws"}://${urlobj.host}/server`)
 		}
 	}
 	window.WebSocket = PrivateServer
