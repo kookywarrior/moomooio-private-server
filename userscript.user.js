1ffda625e33200fdd4ea64098bdf3c9fd9804bf8
KOOKY WARRIOR <91371138+kookywarrior@users.noreply.github.com>
2023-11-03 08:27:53 +0800

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -3,37 +3,30 @@
 // @description  Let you connect to the private server
 // @icon         https://moomoo.io/img/favicon.png?v=1
 // @author       KOOKY WARRIOR
-// @version      0.1.1
+// @version      0.2
 // @match        *://*.moomoo.io/*
 // @grant        none
 // ==/UserScript==
 
 ;(async () => {
-	function getLocation(href) {
-		if (!href) return null
-		var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/)
-		return (
-			match && {
-				href: href,
-				protocol: match[1],
-				host: match[2],
-				hostname: match[3],
-				port: match[4],
-				pathname: match[5],
-				search: match[6],
-				hash: match[7]
-			}
-		)
+	function getLocation(string) {
+		let url
+		try {
+			url = new URL(string)
+		} catch (_) {
+			return false
+		}
+		return url
 	}
 	const urlobj = getLocation(window.prompt("URL", ""))
 
-	if (!urlobj?.host) return
+	if (!urlobj || !(urlobj.protocol === "http:" || urlobj.protocol === "https:")) return
 
 	window.PrivateServer = true
 	let ws = window.WebSocket
 	class PrivateServer extends ws {
 		constructor() {
-			super(`${urlobj.protocol == "https" ? "wss" : "ws"}://${urlobj.host}/server`)
+			super(`${urlobj.protocol == "https:" ? "wss" : "ws"}://${urlobj.host}/server`)
 		}
 	}
 	window.WebSocket = PrivateServer
